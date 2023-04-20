20f9d3174aa95143bcdeada690d021232442c737
tluyen2602@gmail.com <tluyen2602@gmail.com>
2023-04-21 00:07:29 +0700

    insert

--- mysql_api/main.py
+++ mysql_api/main.py
@@ -1,25 +1,34 @@
-from fastapi import FastAPI
-import mysql.connector
 import pandas as pd
+import mysql.connector
+import csv
 
-app = FastAPI()
-cnx = mysql.connector.connect(user='root', password='psw123', port='6603', host='127.0.0.1', database='foody_db')
-file_path ='../data_crawl/code/data.csv'
+# Kết nối đến cơ sở dữ liệu
+mydb = mysql.connector.connect(user='root', password='psw123', port='6603',
+host='127.0.0.1',
+database='foody_db')
 
-# @app.post("/save_data_delivery_infos")
-# def save_data_delivery_infos():
-# Đọc dữ liệu từ file CSV
-df = pd.read_csv(file_path)
+# Tạo một con trỏ để thao tác với database
+mycursor = mydb.cursor()
 
-# Lưu dữ liệu vào MySQL
-cursor = cnx.cursor()
-for index, row in df.iterrows():
-    query = f"INSERT INTO table_name (delivery_id, delivery_name, phones , delivery_address, rating) VALUES ('{row['column1']}', '{row['column2']}', '{row['column3']}', '{row['column4']}', '{row['column5']}')"
-    cursor.execute(query)
-cnx.commit()
-# return {"message": "Data saved to database successfully!"}
-cursor.close()
-cnx.close()
+# Đọc file CSV
+
+with open('../data_crawl/code/dishes.csv', 'r', encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile)
+    next(csvreader) # bỏ qua dòng tiêu đề
+    for row in csvreader:
+        # xử lý dữ liệu trong mỗi row
+        # ví dụ, lưu dữ liệu vào database
+        sql = "INSERT INTO Dishes (dish_id, dish_name, price,d_description,dish_type_name,delivery_id) VALUES (%s, %s, %s, %s, %s, %s)"
+        val = (row[0], row[1], row[2],row[3],row[4],row[5])
+        mycursor.execute(sql, val)
+
+mydb.commit()
+
+# Đóng kết nối đến cơ sở dữ liệu
+mycursor.close()
+mydb.close()
+
+
 # cnx = mysql.connector.connect(user='root', password='psw123', port='6603', host='127.0.0.1', database='foody_db')
 # cursor = cnx.cursor()
 # table_name = 'Delivery_Infos'
